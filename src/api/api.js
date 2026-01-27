302a3b656ad789c35605127bc9b86d74d245c9b9
Rahul Yadav <yadavrahul9686@gmail.com>
2026-01-27 23:54:15 +0530

    Fix API URL for production (MockAPI)

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/books";
+const API_URL = "https://6978faa1cd4fe130e3dada14.mockapi.io/books";
 
 export const getBooks = () => axios.get(API_URL);
-
-export const getBookById = (id) =>
-  axios.get(`${API_URL}/${id}`);
-
-export const addBook = (data) =>
-  axios.post(API_URL, data);
-
+export const getBookById = (id) => axios.get(`${API_URL}/${id}`);
+export const addBook = (data) => axios.post(API_URL, data);
 export const updateBook = (id, data) =>
   axios.put(`${API_URL}/${id}`, data);
-
 export const deleteBook = (id) =>
   axios.delete(`${API_URL}/${id}`);
